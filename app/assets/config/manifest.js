02b8894f1f9f5450e3ca22fc8c2d54c91b036531
jreyes1990 <jarl1990mt@gmail.com>
2022-08-09 23:01:14 -0600

    Uso de datables, con iconos de descarga y busqueda por campos

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,24 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+
+
+//= link bootstrap/bootstrap.min.css
+//= link datatables/dataTables.bootstrap4.min.css
+//= link datatables/datatables.min.css
+//= link datatables/fixedHeader.dataTables.min.css
+
+//= link main.js
+//= link bootstrap/jquery-3.5.1.js
+//= link bootstrap/popper.min.js
+//= link bootstrap/bootstrap.min.js
+//= link datatables/datatables.min.js
+//= link datatables/dataTables.jqueryui.min.js
+//= link datatables/dataTables.buttons.min.js
+//= link datatables/dataTables.select.min.js
+//= link datatables/jszip.min.js
+//= link datatables/pdfmake.min.js
+//= link datatables/vfs_fonts.js
+//= link datatables/buttons.html5.min.js
+//= link datatables/buttons.print.min.js
+//= link datatables/buttons.colVis.min.js
+//= link datatables/dataTables.fixedHeader.min.js
